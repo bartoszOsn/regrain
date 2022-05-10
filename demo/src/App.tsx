1915f9964d9ee598f39b0ba6ef0f1f171b607d8a
bartoszOsn <osinskibartosz24@gmail.com>
2022-05-10 19:58:14 +0200

    add demo app

---
+++ demo/src/App.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { hot } from "react-hot-loader/root";
+import { createGrain } from "regrain";
+
+interface Props {
+   name:
+    string
+}
+
+createGrain('asd', 5);
+
+class App extends React.Component<Props> {
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <h1 className="text-4xl text-white bg-black">
+          Hello {name}
+        </h1>
+      </>
+    );
+  }
+}
+
+export default hot(App);
---
+++ demo/webpack.config.js
@@ -0,0 +1,63 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.tsx'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'src/index.html' }],
+    })
+  ],
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js'
+    ],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      'regrain': path.resolve(__dirname, '..')
+    }
+  }
+};
+module.exports = config;
