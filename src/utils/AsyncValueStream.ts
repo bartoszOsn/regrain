c1e9db485dd66e02141d44205aed7de98bc7c67d
bartoszOsn <osinskibartosz24@gmail.com>
2022-05-14 20:47:39 +0200

    AsyncValueStream

---
+++ src/utils/AsyncValueStream.ts
@@ -0,0 +1,36 @@
+import { StreamClosedError } from "./StreamClosedError";
+
+export class AsyncValueStream<T> {
+	private queue: Array<T> = [];
+	private onDispatch: (() => void) | null = null;
+	private isClosed: boolean = false;
+
+
+	select(): Promise<T> {
+		if (this.queue.length > 0) {
+			return Promise.resolve(this.queue.shift()!);
+		}
+
+		if (this.isClosed) {
+			throw new StreamClosedError();
+		}
+
+		return new Promise(resolve => {
+			this.onDispatch = () => {
+				resolve(this.queue.shift()!);
+			}
+		});
+	}
+
+	dispatch(value: T): void {
+		this.queue.push(value);
+		if (this.onDispatch) {
+			this.onDispatch();
+		}
+		this.onDispatch = null;
+	}
+
+	close(): void {
+		this.isClosed = true;
+	}
+}
---
+++ tests/utils/AsyncValueStream.spec.ts
@@ -0,0 +1,59 @@
+import { AsyncValueStream } from "../../src/utils/AsyncValueStream";
+import { StreamClosedError } from "../../src/utils/StreamClosedError";
+
+describe('AsyncValueStream', function () {
+	let stream: AsyncValueStream<number>
+
+	beforeEach(() => {
+		stream = new AsyncValueStream<number>();
+	})
+	it('should resolve dispatched value', function (done) {
+		const value = 5;
+
+		stream.dispatch(value);
+
+		stream.select()
+			.then((v) => {
+				expect(v).toBe(value);
+				done();
+			});
+	});
+
+	it('should resolve previously dispatched value', function (done) {
+		const value = 5;
+
+		stream.select()
+			.then((v) => {
+				expect(v).toBe(value);
+				done();
+			});
+
+		stream.dispatch(value);
+	});
+
+	it('should queue values', function (done) {
+		const values = [1, 2, 3];
+		const promises = [];
+
+		promises.push(stream.select());
+		stream.dispatch(values[0]);
+		stream.dispatch(values[1]);
+		stream.dispatch(values[2]);
+		promises.push(stream.select());
+		promises.push(stream.select());
+
+		promises.forEach((promise, i) => promise.then(v => expect(v).toBe(values[i])));
+
+		Promise.all(promises).then(() => done());
+	});
+
+	it('should be closeable', function () {
+		const value = 5;
+
+		stream.dispatch(value);
+		stream.close();
+
+		stream.select()?.then(v => expect(v).toBe(value));
+		expect(() => stream.select()).toThrow(StreamClosedError);
+	});
+});
